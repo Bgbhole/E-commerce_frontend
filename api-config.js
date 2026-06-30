170d8c2a3713e7a1b10f818bbb14a0c2ca85a198
sw21092000-eng <sw21092000@gmail.com>
2026-06-30 21:41:29 +0530

    api updated

--- api-config.js
+++ api-config.js
@@ -2,4 +2,4 @@ const API_BASE_URL =
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
         ? "http://localhost:8080"
-        : "https://your-render-backend.onrender.com";
+           : "https://e-commerce-backend-3rh6.onrender.com";
